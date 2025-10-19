7a7b59679bf58e2fbf38314bdf60f76b3e314a44
sdw98 <sdw9851@gmail.com>
2025-10-19 19:15:20 +0900

    UserDetailsServiceImpl 생성 및 UserDetails 구현

--- src/main/java/com/example/mooddy_Auth/config/ApplicationConfig.java
+++ src/main/java/com/example/mooddy_Auth/config/ApplicationConfig.java
@@ -23,19 +23,6 @@ public class ApplicationConfig {
         return config.getAuthenticationManager();
     }
 
-    // authenticationManager.authenticate() 호출 시 내부에서 이 Bean 사용
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return email -> {
-            return userRepository.findByEmail(email)
-                    .orElseThrow(() ->
-                            new AuthenticationException("No user found with email" + email)
-                    );
-        };
-    }
-
-
-
     // PasswordEncoder Bean 등록
     @Bean
     public PasswordEncoder passwordEncoder() {
---
+++ src/main/java/com/example/mooddy_Auth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.mooddy_Auth.service;
+
+import com.example.mooddy_Auth.entity.User;
+import com.example.mooddy_Auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+// authenticationManager.authenticate() 호출 시 내부에서 이 Bean 사용
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Email not found" + email));
+    }
+}
+
